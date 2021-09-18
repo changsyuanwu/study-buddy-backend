dd6416d447853af62522468cdc4129fc9a2828aa
Andrew Chan <a258chan@uwaterloo.ca>
2021-09-18 19:33:21 -0400

    feat: retrieve question set given a category

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get("/", function (req, res, next) {
   res.send("Hello from App Engine!");
 });
 
+router.get("/getQuestions/:category", async function(req, res, next) {
+  const db = req.db;
+  const category = req.params.category.toLowerCase();
+  console.log(category);
+
+  let questions = [];
+
+  const snapshot = await db.collection(category).get();
+  snapshot.forEach((doc) => {
+    console.log(doc.id, '=>', doc.data());  // debugging tool
+    questions.push(doc.data());
+  });
+
+  res.json(questions);
+});
+
 module.exports = router;
